8b923404f9cb896c029ff6061dc740ee5fcbd61f
PastorRowan <rowanvanzyl@gmail.com>
2025-01-05 14:50:01 +0200

    init

---
+++ App/App/src/renderer/src/App.tsx
@@ -0,0 +1,13 @@
+
+import Card from "./components/Card";
+
+function App(): JSX.Element {
+    return (
+        <div>
+            <h1 className="bg-gray-400">working?</h1>
+            <Card />
+        </div>
+    );
+};
+
+export default App;
---
+++ App/App/src/renderer/src/components/Card.tsx
@@ -0,0 +1,56 @@
+
+import { useState } from "react";
+
+// Define props for the component
+interface CardProps {
+    id: string; // Unique identifier for the card
+    rank: string; // The rank of the card (e.g., "2", "King", "Ace")
+    suit: string; // The suit of the card (e.g., "Hearts", "Spades")
+    startFlipped: boolean; // Whether the card is face-up
+    isSelected?: boolean; // Optional: Whether the card is currently selected
+  } 
+
+// Functional Component in TypeScript
+function Card({
+    id,
+    rank,
+    suit,
+    startFlipped, 
+    isSelected,
+}: CardProps): JSX.Element {
+
+    const [
+        flipped,
+        setFlipped,
+    ] = useState(startFlipped); // State to manage the flip
+
+    // Toggle flip when the card is clicked
+    function handleClick(): void {
+        setFlipped((prev) => !prev);
+    };
+
+    return (
+        <div
+            className=""
+            onClick={handleClick}
+        >
+            {
+                flipped ? (
+                    <div className="aspect-[37/54] w-[222px] h-[324px]">
+                        <img
+                            src="/src/assets/cards/2_of_clubs.svg"
+                        />
+                    </div>
+                ) : (
+                    <div className="aspect-[37/54] w-[222px] h-[324px]">
+                        <img
+                            src="/src/assets/cards/back.svg"
+                        />
+                    </div>
+            )}
+        </div>
+    );
+
+};
+
+export default Card;
